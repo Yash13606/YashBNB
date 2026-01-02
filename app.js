06718ca5fe768646ec379097e017f32bed5d4275
Yash13606 <yashmarlecha1@gmail.com>
2026-01-02 23:07:23 +0530

    Add modern Airbnb-style UI with navbar, footer, and redesigned listing pages

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const Listing = require("./Models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
-
+const ejsMate = require("ejs-mate");    
 app.use(methodOverride("_method"));
 main().then(() => {
     console.log("Connected to MongoDB");
@@ -19,6 +19,8 @@ async function main() {
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
     res.send("This is the home page");
